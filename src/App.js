0855ad588a151eef680ad864505d80b90d3c9c63
Zach-Trani <trani.zach@gmail.com>
2023-12-12 19:25:19 -0500

    Feat: Add state to todo retrieval and new tasks, Add addTodo

--- src/App.js
+++ src/App.js
@@ -2,17 +2,40 @@ import React, { useState, useEffect } from 'react';
 import { supabase } from './supabaseClient';
 
 function App() {
+  
+  const [todos, setTodos] = useState([])
+  const [newTask, setNewTask] = useState('')
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+  
   // accessing DBs/APIs requires async/await
-  const fetchTodosc = async () => {
-    // select * from todos
+  const fetchTodos = async () => {
+    // select * from todos order by id ascending
     const {data, error} = await supabase
     .from('todos')
     .select('*')
     .order('id',{ascending:true})
+    if (error) {
+      console.error('Error fetching todos:.', error)
+    } else {
+      setTodos(data);
+    }
   }
 
   const addTodo = async () => {
-
+    // insert into todos (task, is_completed) values (newTask, false)
+    const {data, error} = await supabase
+    .from('todos')
+    .insert([{task: newTask, is_completed: false}])
+    .select()
+    if (error) {
+      console.error('Error fetching todos:.', error)
+    } else {
+      setTodos(prevTodos => [...prevTodos, ...data])
+      setNewTask('')
+    }
   }
 
   const updateTodo = async () => {
